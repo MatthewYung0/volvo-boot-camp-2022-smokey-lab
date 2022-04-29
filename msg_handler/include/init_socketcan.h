5d31928b7393987ed4591dd957f36217b133869d
Carl Helgegren <43139572+Calld0r@users.noreply.github.com>
2022-04-29 11:32:05 +0200

    Delete msg_handler directory

--- msg_handler/include/init_socketcan.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "socketcan_cpp.h"
-#include "can_message.h"
-
-void InitSocketcan(scpp::SocketCan &sockat_can);
-void SendMessage(scpp::SocketCan &sockat_can, CanMessage &can_msg);
