c8ef51ec8aef17f0cb63060a52538e885280c2f8
Carl Helgegren <carl.helgegren@live.se>
2022-04-29 12:21:07 +0200

    vcan_start.sh added back again

---
+++ vcan_start.sh
@@ -0,0 +1,21 @@
+####################################
+# Script to start 
+# vcan
+####################################
+# Make the bash program executable
+# by +x (adding execute permission to all)
+# chmod +x vcan_start.sh
+####################################
+#!/bin/bash
+
+# Set the name og the can port
+CAN_PORT="engineCAN"
+
+echo "$CAN_PORT started"
+
+# Start vcan
+sudo modprobe vcan
+# add and set up dev with name CAN_PORT
+sudo ip link add dev $CAN_PORT type vcan
+sudo ip link set up $CAN_PORT
+
