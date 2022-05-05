461fb6264f71b420fefafc668c632fa0a1a3bce9
Carl Helgegren <carl.helgegren@live.se>
2022-05-05 23:10:50 +0200

    true libs added, multiple executables now  built in example folder

--- libs/libinputhandler/include/socketcansetup.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "socketcan_cpp.h"
-#include "canmessage.h"
-
-void InitSocketcan(scpp::SocketCan &sockat_can);
-void SendMessage(scpp::SocketCan &sockat_can, CanMessage &can_msg);
-void ReadMessage(scpp::SocketCan &sockat_can, scpp::CanFrame &fr);
