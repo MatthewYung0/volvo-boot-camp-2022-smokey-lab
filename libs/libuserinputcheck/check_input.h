fb9bc32b9bad276358a24612c0881c2d487f5ef1
MatthewYung0 <matthew.yung0@hotmail.com>
2022-05-10 10:04:45 +0200

    added userinputcheck common document

---
+++ libs/libuserinputcheck/check_input.h
@@ -0,0 +1,17 @@
+#define START_STOP 115
+#define GEAR_DRIVE 100
+#define GEAR_REAR 114
+#define GEAR_NEUTRAL 110
+#define GEAR_PARK 112
+#define THROTTLE_0 48
+#define THROTTLE_10 49
+#define THROTTLE_20 50
+#define THROTTLE_30 51
+#define THROTTLE_40 52
+#define THROTTLE_50 53
+#define THROTTLE_60 54
+#define THROTTLE_70 55
+#define THROTTLE_80 56
+#define THROTTLE_90 57
+
+#define APPLICATION_ON_OFF
