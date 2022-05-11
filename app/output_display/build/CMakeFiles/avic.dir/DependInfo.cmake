9f9cf86c6c28fffc51df2234269303b2a702f44d
MatthewYung0 <matthew.yung0@hotmail.com>
2022-05-11 14:34:23 +0200

    adjusted path files in main

---
+++ app/output_display/build/CMakeFiles/avic.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/avic_autogen/mocs_compilation.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/avic_autogen/mocs_compilation.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/include/moc_your_stuff.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/include/moc_your_stuff.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/main.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/main.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/qrc_cluster-gauges.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/qrc_cluster-gauges.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/qrc_images.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/qrc_images.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/src/can_opener.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/src/can_opener.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/src/cluster_updater.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/src/cluster_updater.cpp.o"
+  "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/src/your_stuff.cpp" "/home/myung/repos/volvo-boot-camp-2022-smokey-lab/app/output_display/build/CMakeFiles/avic.dir/src/your_stuff.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QML_LIB"
+  "QT_QUICKCONTROLS2_LIB"
+  "QT_QUICKWIDGETS_LIB"
+  "QT_QUICK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "avic_autogen/include"
+  "../include"
+  "../../../libs/libcanio/include"
+  "../include_directories"
+  "../("
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui/5.12.8"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui/5.12.8/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore/5.12.8"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore/5.12.8/QtCore"
+  "../)"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuickControls2"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuick"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQml"
+  "/usr/include/x86_64-linux-gnu/qt5/QtNetwork"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtQuickWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
