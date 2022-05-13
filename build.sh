e551dfbc61dbcc5ebf714f19e3db7791fe4fdb48
Carl Helgegren <43139572+Calld0r@users.noreply.github.com>
2022-05-13 15:44:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,26 @@
-#!/usr/bin/env bash
-konsole --noclose -e echo Hello terminal 1! &
+####################################
+# Script to build all files
+####################################
+# Make the bash program executable
+# by +x (adding execute permission to all)
+# chmod +x *.sh
+####################################
+#!/bin/bash
+
+mkdir build
+echo "create directory"
+cd build
+echo "move into build"
+cmake ..
+echo "run cmake"
+make
+echo "run make"
+
+# add terminal you use if you dont have
+# mate-terminal
+cd app/emulator/ && gnome-terminal -e ./emulator&
+echo "open emulator terminal"
+cd app/input_handler/ && gnome-terminal -e ./input_handler &
+echo "open input_handler terminal"
+cd app/output_display/ && gnome-terminal -e ./avic -c engineCAN &
+echo "open output display"
