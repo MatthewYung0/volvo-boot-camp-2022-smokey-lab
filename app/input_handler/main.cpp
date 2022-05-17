d5919a518bc081f39754948a0be7f93a1d120599
Carl Helgegren <carl.helgegren@live.se>
2022-05-17 16:12:27 +0200

    graceful shutdown added to emulator and display

--- app/input_handler/main.cpp
+++ app/input_handler/main.cpp
@@ -26,28 +26,31 @@ int main() {
 
     if ((ch = getch()) == ERR) {
     }
-    can_msg.SetFrame(ch);
     if (ch == EXIT) {
-      can_msg.SetFrame(ch);
-
       bool run = true;
       while (run) {
-        for (int i = 0; i < 110; i++) {
+        for (int i = 0; i < 120; i++) {
           can_msg.SetFrame(THROTTLE_0);
           SendMessage(sockat_can, can_msg.frame);
           std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-        can_msg.SetFrame(GEAR_PARK);
+        for (int i = 0; i < 10; i++) {
+          can_msg.SetFrame(GEAR_PARK);
+          SendMessage(sockat_can, can_msg.frame);
+          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
         can_msg.SetFrame(START_STOP);
+        can_msg.SetFrame(ch);
         SendMessage(sockat_can, can_msg.frame);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         run = false;
+        exit(-1);
       }
-
-      exit(-1);
+    } else {
+      can_msg.SetFrame(ch);
+      SendMessage(sockat_can, can_msg.frame);
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    SendMessage(sockat_can, can_msg.frame);
   }
 
   endwin();
