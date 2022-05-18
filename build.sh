dc1f8b68749f5cd3d98588f65be299d2408e5f76
Carl Helgegren <carl.helgegren@live.se>
2022-05-18 14:41:35 +0200

    added commands to run valgrind from bash build script

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ mkdir build
 echo "create directory"
 cd build
 echo "move into build"
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 echo "run cmake"
 make
 echo "run make"
@@ -21,9 +21,9 @@ cd ..
 # mate-terminal
 pwd
 
-cd build/app/emulator/ && gnome-terminal --geometry 50x12+0-0 -e ./emulator &
+cd build/app/emulator/ && gnome-terminal --geometry 50x12+0-0 --command="valgrind --leak-check=yes --log-file=valgrind_emulator.txt ./emulator" &
 echo "open emulator terminal"
-cd build/app/input_handler/ && gnome-terminal --geometry 60x12+430-0 -e ./input_handler &
+cd build/app/input_handler/ && gnome-terminal --geometry 60x12+430-0 --command="valgrind --leak-check=yes --log-file=valgrind_input_handler.txt ./input_handler" &
 echo "open input_handler terminal"
 cd build/app/output_display/ && gnome-terminal --geometry 60x12+930-0   --command="./avic -c engineCAN" &
 echo "open output display"
