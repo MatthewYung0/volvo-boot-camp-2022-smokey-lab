2c2262ff7f0e59ac71787d936208ca884141f87a
Carl Helgegren <carl.helgegren@live.se>
2022-05-19 20:02:44 +0200

    added text output from input handler

--- app/input_handler/src/canmessage.cpp
+++ app/input_handler/src/canmessage.cpp
@@ -31,58 +31,107 @@ void CanMessage::SetPedalPos(int _pedal_pos) {
   this->frame.data[user_frame_data_partition::PEDAL_POS] = _pedal_pos;
 }
 void CanMessage::InitiateGracefulShutdown() {
-  this->frame.data[user_frame_data_partition::EXIT] = keyboard_common_input::EXIT;
+  this->frame.data[user_frame_data_partition::EXIT] =
+      keyboard_common_input::EXIT;
 }
 
 void CanMessage::SetFrame(int _input) {
-  std::cout << _input << "\n\r";
+  // std::cout << _input << "\n\r";
   switch (_input) {
   case keyboard_input::START_STOP:
+    std::cout << "Key pressed: " << _input << " | "
+              << "START / STOP"
+              << "\n\r";
     CanMessage::SetIgnition();
     break;
   case keyboard_input::GEAR_DRIVE:
+    std::cout << "Key pressed: " << _input << " | "
+              << "GEAR DRIVE"
+              << "\n\r";
     CanMessage::SetGearDrive();
     break;
   case keyboard_input::GEAR_REAR:
+    std::cout << "Key pressed: " << _input << " | "
+              << "GEAR REVERSE"
+              << "\n\r";
     CanMessage::SetGearRear();
     break;
   case keyboard_input::GEAR_NEUTRAL:
+    std::cout << "Key pressed: " << _input << " | "
+              << "GEAR NEUTRAL"
+              << "\n\r";
     CanMessage::SetGearNeutral();
     break;
   case keyboard_input::GEAR_PARK:
+    std::cout << "Key pressed: " << _input << " | "
+              << "GEAR PARK"
+              << "\n\r";
     CanMessage::SetGearPark();
     break;
   case keyboard_input::THROTTLE_0:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 0%"
+              << "\n\r";
     CanMessage::SetPedalPos(0);
     break;
   case keyboard_input::THROTTLE_10:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 10%"
+              << "\n\r";
     CanMessage::SetPedalPos(10);
     break;
   case keyboard_input::THROTTLE_20:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 20%"
+              << "\n\r";
     CanMessage::SetPedalPos(20);
     break;
   case keyboard_input::THROTTLE_30:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 30%"
+              << "\n\r";
     CanMessage::SetPedalPos(30);
     break;
   case keyboard_input::THROTTLE_40:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 40%"
+              << "\n\r";
     CanMessage::SetPedalPos(40);
     break;
   case keyboard_input::THROTTLE_50:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 50%"
+              << "\n\r";
     CanMessage::SetPedalPos(50);
     break;
   case keyboard_input::THROTTLE_60:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 60%"
+              << "\n\r";
     CanMessage::SetPedalPos(60);
     break;
   case keyboard_input::THROTTLE_70:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 70%"
+              << "\n\r";
     CanMessage::SetPedalPos(70);
     break;
   case keyboard_input::THROTTLE_80:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 80%"
+              << "\n\r";
     CanMessage::SetPedalPos(80);
     break;
   case keyboard_input::THROTTLE_90:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "THROTTLE: 90%"
+              << "\n\r";
     CanMessage::SetPedalPos(90);
     break;
   case keyboard_common_input::EXIT:
+    std::cout << "Key pressed: " << _input << "  | "
+              << "EXIT APPLICATION"
+              << "\n\r";
     CanMessage::InitiateGracefulShutdown();
     break;
 
